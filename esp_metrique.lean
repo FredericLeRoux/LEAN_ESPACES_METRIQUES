3bc3d9a11ed6b4cd9face42a65b24bbf931afbc4
Frederic Le Roux <frederic.le-roux@imj-prg.fr>
2019-12-10 10:31:52 +0100

    preuve de l'intersection d'ouverts

--- esp_metrique.lean
+++ esp_metrique.lean
@@ -92,16 +92,37 @@ begin
   exact BC (AB a_app_A),  
 end
 
-
-
-
+-- ** Une preuve claire
+/-- Une union d'ouverts d'un espace métrique est un ouvert -/
 lemma union_ouverts_est_ouvert (I : set (set X)) : (∀ O ∈ I, ouvert O) → ouvert (⋃₀ I) :=
+begin
+  -- Supposons que tous les O dans I sont ouverts.
+  intro O_ouverts,
+  -- Soit x un point d'un des O dans I
+  rintro x ⟨O, O_app_I, x_app_O⟩,
+  -- Comme O est ouvert, il existe r > 0 tel que B(x, r) ⊆ O
+  obtain ⟨r, r_positif, boule_dans_O⟩ : ∃ r > 0, boule x r ⊆ O,
+    from (O_ouverts O) O_app_I x x_app_O,
+  -- Montrons que ce r convient 
+  use [r, r_positif],
+  -- Puisque  B(x, r) ⊆ O, il suffit de montrer que O ⊆ ⋃₀ I
+  transitivity O, assumption,
+  -- Or O est dans I.
+  exact subset_sUnion_of_mem O_app_I
+end
+
+-- ** variante en λ-calcul
+lemma union_ouverts_est_ouvert' (I : set (set X)) : (∀ O ∈ I, ouvert O) → ouvert (⋃₀ I) :=
+assume O_ouverts x ⟨O, O_app_I, x_app_O⟩,
+let ⟨r, r_positif, boule_dans_O⟩ := O_ouverts O O_app_I x x_app_O in
+⟨r, r_positif, subset.trans boule_dans_O (subset_sUnion_of_mem O_app_I)⟩
+
+-- ** Une preuve moins claire...
+lemma union_ouverts_est_ouvert'' (I : set (set X)) : (∀ O ∈ I, ouvert O) → ouvert (⋃₀ I) :=
 begin
   intro O_ouverts, -- les ensembles O sont ouverts
   intros x x_dans_union, -- un point dans l'union
-  cases x_dans_union with O x_app_O', -- donc dans l'un des ouverts O
-  rw exists_prop at x_app_O',
-  cases x_app_O' with O_app_I x_app_O,
+  rcases x_dans_union with ⟨O, O_app_I, x_app_O⟩, 
   have O_ouvert : ouvert O, from (O_ouverts O) O_app_I,
   have existe_r, from O_ouvert x x_app_O, -- O est ouvert, ce qui fourni un rayon
   cases existe_r with r hr,
@@ -122,21 +143,55 @@ end
 
 -- on montre que l'intersection de deux ouvert est un ouvert, 
 -- puis intersection finie par récurrence (ou directement)
-lemma intersection_deux_ouverts_est_ouvert : ∀ O₁ O₂ : set X, (ouvert O₁ ∧ ouvert O₂) → ouvert (O₁ ∩ O₂) :=
+/-- L'intersecrion de deux ouverts est un ouvert -/
+lemma intersection_deux_ouverts_est_ouvert : ∀ O₁ O₂ : set X, ouvert O₁ →  ouvert O₂ → ouvert (O₁ ∩ O₂) :=
 begin
-sorry
+  -- Soit x un point dans l'intersection,
+  rintro O₁ O₂ ouvert_O₁ ouvert_O₂ x ⟨x_app_O₁,x_app_O₂⟩,
+  -- le fait que O₁ et O₂ soient ouverts fournis deux nombres positifs
+  obtain ⟨r₁,r₁_pos,boule_dans_O₁⟩ : ∃ r₁>0, boule x r₁ ⊆ O₁, from ouvert_O₁ x x_app_O₁,
+  obtain ⟨r₂,r₂_pos,boule_dans_O₂⟩ : ∃ r₂>0, boule x r₂ ⊆ O₂, from ouvert_O₂ x x_app_O₂,
+  -- Montrons que le minimum r des deux convient
+  use min r₁ r₂, 
+  rw exists_prop,
+  -- Il est bien positif
+  split, 
+    by exact lt_min r₁_pos r₂_pos,
+  -- Prenons un y dans la boule de rayon r
+  intros y y_app_boule, 
+  simp [boule] at y_app_boule,
+  rcases y_app_boule with ⟨ineg_1,ineg_2⟩,
+  -- il est dans O₁ et dans O₂ 
+  have y_O₁ : y ∈ O₁, from boule_dans_O₁ ineg_1,
+  have y_O₂ : y ∈ O₂, from boule_dans_O₂ ineg_2,
+  -- donc dans l'intersection, comme voulu.
+  exact and.intro y_O₁ y_O₂,
 end
 
 
 variable β : Type
+/-- L'intersecrion d'un nombre fini d'ouverts est un ouvert -/
 lemma intersection_ouverts_est_ouvert  [fintype β] {O : β → set X}
   (h : ∀ i, ouvert (O i)) : ouvert (⋂ i, O i) :=
 begin
 sorry
 end
 
--- comment formuler que X et ∅ sont ouverts ?
+lemma vide_ouvert : ouvert (∅ : set X) :=
+begin
+  intros x x_in,
+  exfalso,
+  exact x_in,
+end
 
+lemma vide_ouvert' : ouvert (∅ : set X) :=
+assume x x_in, false.elim x_in
+
+
+lemma total_ouvert : ouvert (univ : set X) :=
+begin
+  sorry
+end
 
 -- def topo de l'intérieur. Caractérisation métrique.
 
@@ -155,8 +210,59 @@ variables {X Y : Type} [espace_metrique X] [espace_metrique Y]
 def continue_en (f : X → Y) (x₀ : X) :=
   ∀ ε > 0, ∃ δ > 0, ∀ x, d_[X] x x₀ < δ → d_[Y] (f x) (f x₀) < ε   
 
-def continuite (f:X → Y) := ∀ x : X, continue_en f x
+def continue (f:X → Y) := 
+∀ x : X, continue_en f x
 
 -- Notations f continue, f continue_au_point x
 
+-- caractérisation topologique (ponctuelle, globale)
+lemma continuite_ouverts (f:X → Y): continue f ↔ ( ∀O, ouvert O → ouvert (f ⁻¹' O) ) :=
+begin
+  sorry
+end
+
+-- composition (ponctuelle, globale)
+
+def lipschitzienne (k:ℝ) (f: X → Y) := 
+∀ x₀  x₁ , d_[Y] (f x₀) (f x₁) ≤ ( k * d_[X] x₀  x₁ )
+
+-- lipschitzien implique continu
+
+
 end continuite
+
+
+----------------------------------------------------
+section fermes
+variables {X:Type} [espace_metrique X]
+
+def ferme (F : set X) := ouvert (- F) 
+
+-- intersection, union
+
+-- adhérence
+
+
+
+
+
+
+
+
+end fermes
+
+
+----------------------------------------------------
+section suites
+variables {X:Type} [espace_metrique X]
+
+
+
+
+
+
+end suites
+
+
+
+
